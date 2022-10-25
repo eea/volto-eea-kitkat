49af0a22f50c65b0eb009bc290fafdcc7bf39b0b
Nilesh <nileshgulia@gmail.com>
2022-10-25 19:18:14 +0530

    remove @eeacms/volto-slate, latest volto compatibility refs #153447 (#62)
    * remove(volto-slate): refs #153447
    
    * update(jest): add @plone/volto-slate resolver refs- #153447
    
    * migrate cypress configs
    
    * JENKINS: use latest volto alppha
    
    * JENKINS: don't run volto-slate cypress
    
    * JENKINS: amend latest changes, remove cypress-slate coverages
    
    * test(cypress): Upgrade to cypress 10
    
    * docs(volto-slate): Add upgrade docs
    
    Co-authored-by: Alin Voinea <contact@avoinea.com>
    Co-authored-by: Miu Razvan <miu.razvan28@gmail.com>

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -9,18 +9,18 @@ module.exports = {
     '@plone/volto/babel': '<rootDir>/node_modules/@plone/volto/babel',
     '@plone/volto/(.*)$': '<rootDir>/node_modules/@plone/volto/src/$1',
     '@package/(.*)$': '<rootDir>/src/$1',
+    '@root/(.*)$': '<rootDir>/src/$1',
     '@plone/volto-quanta/(.*)$': '<rootDir>/src/addons/volto-quanta/src/$1',
     '@eeacms/(.*?)/(.*)$': '<rootDir>/src/addons/$1/src/$2',
-    'volto-slate/(.*)$': '<rootDir>/src/addons/volto-slate/src/$1',
+    '@plone/volto-slate':
+      '<rootDir>/node_modules/@plone/volto/packages/volto-slate/src',
     '~/(.*)$': '<rootDir>/src/$1',
     'load-volto-addons':
       '<rootDir>/node_modules/@plone/volto/jest-addons-loader.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
   transform: {
     '^.+\\.js(x)?$': 'babel-jest',
-    '^.+\\.css$': 'jest-css-modules',
-    '^.+\\.less$': 'jest-css-modules',
-    '^.+\\.scss$': 'jest-css-modules',
     '^.+\\.(png)$': 'jest-file',
     '^.+\\.(jpg)$': 'jest-file',
     '^.+\\.(svg)$': './node_modules/@plone/volto/jest-svgsystem-transform.js',

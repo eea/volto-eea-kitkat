b00bc07f1d7627d30bf1576fcb87b59e560808a2
ana-oprea <anaop3212@gmail.com>
2023-07-12 14:12:05 +0300

    chore: fix ESlint - refs #254313

--- src/customizations/volto/components/manage/Controlpanels/VersionOverview.test.jsx
+++ src/customizations/volto/components/manage/Controlpanels/VersionOverview.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import VersionOverview from './VersionOverview';
 import config from '@plone/volto/registry';
 import '@testing-library/jest-dom/extend-expect';

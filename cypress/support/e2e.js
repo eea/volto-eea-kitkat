ec825e6250a54f8b062b52420ba41c89cb8da8a3
dana-cfc4 <dana.comiselu4@gmail.com>
2024-03-12 14:57:26 +0200

    test: comment waitForResourceToLoad in cypress support

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -22,7 +22,7 @@ import './commands';
 import '@cypress/code-coverage/support';
 
 // Fail Fast
-import "cypress-fail-fast";
+import 'cypress-fail-fast';
 
 export const slateBeforeEach = (contentType = 'Document') => {
   cy.autologin();
@@ -38,11 +38,11 @@ export const slateBeforeEach = (contentType = 'Document') => {
     path: 'cypress',
   });
   cy.visit('/cypress/my-page');
-  cy.waitForResourceToLoad('@navigation');
-  cy.waitForResourceToLoad('@breadcrumbs');
-  cy.waitForResourceToLoad('@actions');
-  cy.waitForResourceToLoad('@types');
-  cy.waitForResourceToLoad('my-page');
+  // cy.waitForResourceToLoad('@navigation');
+  // cy.waitForResourceToLoad('@breadcrumbs');
+  // cy.waitForResourceToLoad('@actions');
+  // cy.waitForResourceToLoad('@types');
+  // cy.waitForResourceToLoad('my-page');
   cy.navigate('/cypress/my-page/edit');
 };
 
